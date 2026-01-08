ca9e29c7b13b2b8eef1a5c21ae876130eec21e79
sandula-sanchana <sandulasanchana@gmail.com>
2026-01-08 15:00:52 +0530

    bean life cycle

--- 02_spring_bean_lifeCycle/src/main/java/edu/ijse/AppInitializer2.java
+++ 02_spring_bean_lifeCycle/src/main/java/edu/ijse/AppInitializer2.java
@@ -1,8 +1,25 @@
 package edu.ijse;
 
 
+import edu.ijse.bean.SpringBean;
+import edu.ijse.config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class AppInitializer2 {
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(AppConfig.class);
+        context.refresh();
+
+
+        SpringBean springBean1=context.getBean(SpringBean.class);
+        System.out.println("springBean1:"+springBean1);
+
+
+
+
+
+        context.registerShutdownHook();
 
     }
 }
--- 02_spring_bean_lifeCycle/src/main/java/edu/ijse/bean/SpringBean.java
+++ 02_spring_bean_lifeCycle/src/main/java/edu/ijse/bean/SpringBean.java
@@ -1,10 +1,37 @@
 package edu.ijse.bean;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SpringBean {
+public class SpringBean implements BeanNameAware, BeanFactoryAware, ApplicationContextAware , DisposableBean {
     public SpringBean() {
-        System.out.println("spring bean CREATED");
+        System.out.println("spring bean CREATED");//instantiation
+    }
+
+    // no method for populate properties
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("setBeanFactory"); //bean factory aware
+    }
+
+    @Override
+    public void setBeanName(String name) {
+       System.out.println("setBeanName");// name aware
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+       System.out.println("setApplicationContext");// application context aware
+    }
+
+    @Override
+    public void destroy() throws Exception {
+       System.out.println("destroy");//disposable bean // cant see this cause u need to add shut down hook
     }
 }
