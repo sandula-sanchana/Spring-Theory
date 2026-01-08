5791b2e908ae17e7edf6dd30aac6975d36db7292
sandula-sanchana <sandulasanchana@gmail.com>
2026-01-08 14:44:17 +0530

    MODULE 2

---
+++ 02_spring_bean_lifeCycle/src/main/java/edu/ijse/AppInitializer2.java
@@ -0,0 +1,8 @@
+package edu.ijse;
+
+
+public class AppInitializer2 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 02_spring_bean_lifeCycle/src/main/java/edu/ijse/bean/SpringBean.java
@@ -0,0 +1,10 @@
+package edu.ijse.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBean {
+    public SpringBean() {
+        System.out.println("spring bean CREATED");
+    }
+}
