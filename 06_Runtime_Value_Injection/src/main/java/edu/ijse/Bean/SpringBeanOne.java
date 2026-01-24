3de1ed9251c602a62ee6abe204a3a9fc5e88a7a7
sandula-sanchana <sandulasanchana@gmail.com>
2026-01-24 14:13:35 +0530

    module 6 runtime value injection

---
+++ 06_Runtime_Value_Injection/src/main/java/edu/ijse/Bean/SpringBeanOne.java
@@ -0,0 +1,15 @@
+package edu.ijse.Bean;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+
+public class SpringBeanOne  {
+
+    public SpringBeanOne(){
+        System.out.println("bean  created");
+    }
+
+
+}
