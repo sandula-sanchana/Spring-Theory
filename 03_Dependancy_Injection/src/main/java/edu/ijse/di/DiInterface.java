fd378d1f5715bab174547ee42b652d8b7618d61f
sandula-sanchana <sandulasanchana@gmail.com>
2026-01-24 11:01:07 +0530

    INTERFACE THROUGHT

---
+++ 03_Dependancy_Injection/src/main/java/edu/ijse/di/DiInterface.java
@@ -0,0 +1,8 @@
+package edu.ijse.di;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public interface DiInterface {
+
+    void inject(DI di);
+}
--- 03_Dependancy_Injection/src/main/java/edu/ijse/di/TestOne.java
+++ 03_Dependancy_Injection/src/main/java/edu/ijse/di/TestOne.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TestOne {
+public class TestOne implements DiInterface {
     DI di;
 
    // @Autowired -not required for constructor through injection
@@ -12,12 +12,18 @@ public class TestOne {
 //       this.di=di;
 //    }
 
-    @Autowired
-    public void setDi(DI di){
-        this.di=di;
-    }
+//    @Autowired
+//    public void setDi(DI di){
+//        this.di=di;
+//    }
 
   public void chatWithTwo(){
       di.chat();
   }
+
+    @Override
+    @Autowired
+    public void inject(DI di) {
+       this.di=di;
+    }
 }
