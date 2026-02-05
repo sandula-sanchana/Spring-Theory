f46970809421c57c08fd91afe48de2c092826475
sandula-sanchana <sandulasanchana@gmail.com>
2026-02-05 15:50:10 +0800

    loose coupling

--- backend/src/main/java/edu/ijse/backend/BackendApplication.java
+++ backend/src/main/java/edu/ijse/backend/BackendApplication.java
@@ -3,7 +3,7 @@ package edu.ijse.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //configuration,enable autoconfiguration,component scan
 public class BackendApplication {
 
     public static void main(String[] args) {
--- backend/src/main/java/edu/ijse/backend/controller/CustomerController.java
+++ backend/src/main/java/edu/ijse/backend/controller/CustomerController.java
@@ -1,6 +1,10 @@
-package edu.ijse.backend;
+package edu.ijse.backend.controller;
 
 
+import edu.ijse.backend.dto.CustomerDTO;
+import edu.ijse.backend.service.CustomerInterface;
+import edu.ijse.backend.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/customer")
 public class CustomerController {
 
+    private final CustomerInterface customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
     @PostMapping
     public void saveCustomer(@RequestBody CustomerDTO customerDTO){
         System.out.println("save customer");
-        System.out.println(customerDTO.getId());
-        System.out.println(customerDTO.getName());
-        System.out.println(customerDTO.getAddress());
+        customerService.saveCustomer(customerDTO);
+
     }
 
 }
