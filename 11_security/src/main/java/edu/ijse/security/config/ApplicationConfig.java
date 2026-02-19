56608f9d43c0a374b817b419119c6dfb877049d1
sandula-sanchana <sandulasanchana@gmail.com>
2026-02-19 17:50:05 +0800

    csrf

---
+++ 11_security/src/main/java/edu/ijse/security/config/ApplicationConfig.java
@@ -0,0 +1,37 @@
+package edu.ijse.security.config;
+
+import edu.ijse.security.repo.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepo userRepo;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepo.findByUsername(username)
+                .map(u -> new User(
+                        u.getUsername(),
+                        u.getPassword(),
+                        List.of(new SimpleGrantedAuthority("ROLE_" + u.getRole().name()))
+                ))
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+    }
+}
